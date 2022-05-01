b8b03b8d6167bb03aa72154a37a45a40c7b70aa7
Haydn Diniz <hadiniz1@sheffield.ac.uk>
2022-05-01 15:25:10 +0100

    Update the navigation bar - Add login, signup, logout and edit profile links on the navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/sessions#new'
   end
   root to: 'dashboards#index'
 end
